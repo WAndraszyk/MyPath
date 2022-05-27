0201105700fd1f17b799f8b8973111c31936b03b
Witold <wandraszyk73@gmail.com>
2022-05-28 01:54:15 +0200

    In progress

--- app/src/main/java/com/example/fragmenty/MainActivity.kt
+++ app/src/main/java/com/example/fragmenty/MainActivity.kt
@@ -5,12 +5,17 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.viewpager2.widget.ViewPager2
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         if((resources.configuration.screenLayout and Configuration.SCREENLAYOUT_SIZE_MASK) < Configuration.SCREENLAYOUT_SIZE_LARGE) {
             val toolbar = findViewById<Toolbar>(R.id.toolbar)
             setSupportActionBar(toolbar)
--- app/src/main/java/com/example/fragmenty/RouteListFragment.kt
+++ app/src/main/java/com/example/fragmenty/RouteListFragment.kt
@@ -5,98 +5,50 @@ import android.content.Intent
 import android.content.res.Configuration
 import android.os.AsyncTask
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.RelativeLayout
+import android.widget.TableLayout
 import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.FragmentTransaction
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import java.sql.DriverManager
+import java.text.FieldPosition
 
 
 class RouteListFragment() : Fragment(R.layout.fragment_route_list) {
-    val routes = mutableListOf<Route>()
-    private val names = mutableListOf<String>()
-    private val images = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val db = DBHelper()
-        val exe = db.execute().get()
-
-        for (i in routes) {
-            names.add(i.getName())
-            images.add(i.getImage())
-        }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val namesListView = view.findViewById(R.id.name_list) as ListView
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val adapter = ViewPageAdapter(activity as MainActivity)
 
-        val r = if((resources.configuration.screenLayout and Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_LARGE){
-            (view.parent as ViewGroup).parent as LinearLayoutCompat
-        }else{
-            (view.parent as ViewGroup).parent as RelativeLayout
+        val pager = view?.findViewById<ViewPager2>(R.id.pager)
+        pager?.adapter = adapter
+
+        val tableLayout = view?.findViewById<TableLayout>(R.id.tabs)
+
+        if(pager !is null && tableLayout !is null){
+            TabLayoutMediator(tableLayout, pager) { tab, position ->
+                tab.text = "Tab " + (position + 1)
+            }.attach()
         }
 
-        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
-            requireContext(), android.R.layout.simple_list_item_1, names)
-        namesListView.adapter = adapter
-        namesListView.setOnItemClickListener { parent, _, position, _ ->
-            parent.getItemAtPosition(position) // The item that was clicked
-            val fragmentContainer = r.findViewById<FragmentContainerView>(R.id.fragment_container)
-            if(fragmentContainer != null){
-                val details = RouteDetailFragment()
-                val ft = parentFragmentManager.beginTransaction()
-                setRoute(position)
-                ft.replace(R.id.fragment_container, details)
-                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                ft.addToBackStack(null)
-                ft.commit()
-            }else{
-                val intent = Intent(requireContext(), DetailActivity()::class.java)
-                intent.putExtra("routeId", position)
-                startActivity(intent)
-            }
-        }
-    }
-
-    private fun setRoute(id: Int){
-        val sharedScore = activity?.getSharedPreferences("com.example.fragmenty.shared",0)
-        val edit = sharedScore?.edit()
-        edit?.putInt("id", id)
-        edit?.apply()
-    }
-
-    @SuppressLint("StaticFieldLeak")
-    inner class DBHelper : AsyncTask<Void, Void, Void>() {
-        var error = ""
-
-        @Deprecated("Deprecated in Java")
-        override fun doInBackground(vararg p0: Void?): Void? {
-            try{
-                Class.forName("com.mysql.jdbc.Driver").newInstance()
-                val url= "jdbc:mysql://10.0.2.2:3306/fragmenty"
-                val connection = DriverManager.getConnection(url, "root","haslo")
-                val statement = connection.createStatement()
-                val resultSet = statement.executeQuery("select * from routes order by name;")
-                while(resultSet.next()){
-                    val name = resultSet.getString(1)
-                    val way = resultSet.getString(2)
-                    val image = resultSet.getString(3)
-                    routes.add(Route(name, way, image))
-                }
-            }catch (e: Exception){
-                error = e.toString()
-                println("----------------DATABASE ERROR: $error--------------")
-            }
-
-            return null
-        }
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 }
+
+
