5df73532b12024b9a6da34d826db474644d03de9
Witold <wandraszyk73@gmail.com>
2022-05-27 16:43:22 +0200

    Custom toolbar

--- app/src/main/java/com/example/fragmenty/MainActivity.kt
+++ app/src/main/java/com/example/fragmenty/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fragmenty
 
 import android.content.res.Configuration
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         if((resources.configuration.screenLayout and Configuration.SCREENLAYOUT_SIZE_MASK) < Configuration.SCREENLAYOUT_SIZE_LARGE) {
             val toolbar = findViewById<Toolbar>(R.id.toolbar)
             setSupportActionBar(toolbar)
-            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.purple_500)))
         }
     }
 }
