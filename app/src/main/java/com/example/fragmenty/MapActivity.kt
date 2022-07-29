e883e1aff216f6015c1838e9346701ac94d8cb13
Stanisław Bilewski <stanislaw.bilewski@gmail.com>
2022-07-29 15:22:51 +0200

    Re-added the compass

--- app/src/main/java/com/example/fragmenty/MapActivity.kt
+++ app/src/main/java/com/example/fragmenty/MapActivity.kt
@@ -14,6 +14,7 @@ import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -37,8 +38,9 @@ import java.net.URL
 import java.sql.DriverManager
 
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback /*, SensorEventListener */ {   // , LocationListener
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {   // , LocationListener
 
+    private lateinit var mCompass: ImageView
     private lateinit var mMapFragment: SupportMapFragment
     private lateinit var mLoading: ProgressBar
     private lateinit var mMap: GoogleMap
@@ -46,26 +48,25 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback /*, SensorEventListe
     private var mPath = mutableListOf<LatLng>()
     private var mNames = mutableListOf<String>()
 
-//    private lateinit var sensorManager: SensorManager
-//    private lateinit var gsensor: Sensor
-//    private lateinit var msensor: Sensor
-//
-//    private val mGravity = FloatArray(3)
-//    private val mGeomagnetic = FloatArray(3)
-//    private val arrR = FloatArray(9)
-//    private val arrI = FloatArray(9)
-//
-//    private var azimuth = 0f
-//    private val azimuthFix = 0f
+    private lateinit var sensorManager: SensorManager
+    private lateinit var gsensor: Sensor
+    private lateinit var msensor: Sensor
 
+    private val mGravity = FloatArray(3)
+    private val mGeomagnetic = FloatArray(3)
+    private val arrR = FloatArray(9)
+    private val arrI = FloatArray(9)
+
+    private var azimuth = 0f
+//    private val azimuthFix = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val db = DBHelper()
         val exe = db.execute().get()
 
-     binding = ActivityMapBinding.inflate(layoutInflater)
-     setContentView(binding.root)
+         binding = ActivityMapBinding.inflate(layoutInflater)
+         setContentView(binding.root)
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
@@ -76,21 +77,23 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback /*, SensorEventListe
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         mLoading = this.findViewById(R.id.MapLoading)
-//
-//        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-//
-//        gsensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-//        msensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
-//
-//        sensorManager.registerListener(this, gsensor,
-//            SensorManager.SENSOR_DELAY_GAME)
-//        sensorManager.registerListener(this, msensor,
-//            SensorManager.SENSOR_DELAY_GAME)
+        mCompass = this.findViewById(R.id.compassImageView)
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        gsensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        msensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+
+        sensorManager.registerListener(this, gsensor,
+            SensorManager.SENSOR_DELAY_GAME)
+        sensorManager.registerListener(this, msensor,
+            SensorManager.SENSOR_DELAY_GAME)
 
         mMapFragment =
             supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
 
         mMapFragment.view?.visibility = View.GONE
+        mCompass.visibility = View.GONE
 
         val origin: LatLng = mPath[0]
         val dest: LatLng = mPath.last()
@@ -235,6 +238,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback /*, SensorEventListe
             // Drawing polyline in the Google Map for the entire route
             mMap.addPolyline(lineOptions)
             mLoading.visibility = View.GONE
+            mCompass.visibility = View.VISIBLE
             mMapFragment.view?.visibility = View.VISIBLE
         }
 
@@ -339,46 +343,39 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback /*, SensorEventListe
         }
     }
 
-//    fun UpdateBearing(){
-//        val cameraPosition = CameraPosition.Builder()
-//            .target(mMap.cameraPosition.target)
-//            .bearing(azimuth)
-//            .zoom(mMap.cameraPosition.zoom)
-//            .build()
-//        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
-//    }
+    override fun onSensorChanged(event: SensorEvent) {
+        val alpha = 0.97f
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            mGravity[0] = alpha * mGravity[0] + (1 - alpha) * event.values[0]
+            mGravity[1] = alpha * mGravity[1] + (1 - alpha) * event.values[1]
+            mGravity[2] = alpha * mGravity[2] + (1 - alpha) * event.values[2]
 
-//    override fun onSensorChanged(event: SensorEvent) {
-//        val alpha = 0.97f
-//        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
-//            mGravity[0] = alpha * mGravity[0] + (1 - alpha) * event.values[0]
-//            mGravity[1] = alpha * mGravity[1] + (1 - alpha) * event.values[1]
-//            mGravity[2] = alpha * mGravity[2] + (1 - alpha) * event.values[2]
-//
-//            val success = SensorManager.getRotationMatrix(arrR, arrI, mGravity, mGeomagnetic)
-//            if (success) {
-//                val orientation = FloatArray(3)
-//                SensorManager.getOrientation(arrR, orientation)
-//                azimuth = Math.toDegrees(orientation[0].toDouble()).toFloat() // orientation
-//                azimuth = (azimuth + azimuthFix + 360) % 360
-//            }
-//        }
-//        if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
-//            mGeomagnetic[0] = alpha * mGeomagnetic[0] + (1 - alpha) * event.values[0]
-//            mGeomagnetic[1] = alpha * mGeomagnetic[1] + (1 - alpha) * event.values[1]
-//            mGeomagnetic[2] = alpha * mGeomagnetic[2] + (1 - alpha) * event.values[2]
-//
-//            val success = SensorManager.getRotationMatrix(arrR, arrI, mGravity, mGeomagnetic)
-//            if (success) {
-//                val orientation = FloatArray(3)
-//                SensorManager.getOrientation(arrR, orientation)
-//                azimuth = Math.toDegrees(orientation[0].toDouble()).toFloat() // orientation
-//                azimuth = (azimuth + azimuthFix + 360) % 360
-//            }
-//        }
-//    }
-//
-//    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-//        return
-//    }
+            val success = SensorManager.getRotationMatrix(arrR, arrI, mGravity, mGeomagnetic)
+            if (success) {
+                val orientation = FloatArray(3)
+                SensorManager.getOrientation(arrR, orientation)
+                azimuth = Math.toDegrees(orientation[0].toDouble()).toFloat() // orientation
+                azimuth = (azimuth + 360) % 360
+                mCompass.rotation = -azimuth
+            }
+        }
+        if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            mGeomagnetic[0] = alpha * mGeomagnetic[0] + (1 - alpha) * event.values[0]
+            mGeomagnetic[1] = alpha * mGeomagnetic[1] + (1 - alpha) * event.values[1]
+            mGeomagnetic[2] = alpha * mGeomagnetic[2] + (1 - alpha) * event.values[2]
+
+            val success = SensorManager.getRotationMatrix(arrR, arrI, mGravity, mGeomagnetic)
+            if (success) {
+                val orientation = FloatArray(3)
+                SensorManager.getOrientation(arrR, orientation)
+                azimuth = Math.toDegrees(orientation[0].toDouble()).toFloat() // orientation
+                azimuth = (azimuth + 360) % 360
+                mCompass.rotation = -azimuth
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        return
+    }
 }
